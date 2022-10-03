ef0903c6dfd02f5f59a57e76f2ac9fc3db1f677b
makyia bonner <makyiabonner@gmail.com>
2022-10-02 23:10:45 -0400

    Giving button choices Cleaning code

--- rps-game.js
+++ rps-game.js
@@ -1,36 +1,45 @@
+//turning rock, paper, scissors logos to buttons for each choice
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+const h3 = document.getElementsByTagName("h3")
+
+let cpuWinCount = 0
+let p1WinCount = 0
+let i = 0
 // establishing players and creating the functionality of the game
 function rps(p1,cpu){
-   
     //Create the gameplay choices for rock paper scissors
-let choices = ["rock","paper","scissors"]
-cpu = choices.at(Math.floor(Math.random()*3));
+    let choices = ["rock","paper","scissors"]
+    cpu = choices.at(Math.floor(Math.random()*3));
   
     //round count and rules
-    for(let i =1;i<=5;i++){
-        console.log(`Round ${i}`)
-        let cpuWinCount = 0
-        let p1WinCount = 0
-        switch (p1 + cpu){
-            case "paperrock": "paperrock"
-            case "rockscissors": "rockscissors"
-            case "scissorspaper": "scissorspaper"
-            p1WinCount++
-            
-                console.log(`User Wins ${p1WinCount}:${cpuWinCount}`);
-                break;
-            case "rockpaper": "rockpaper"
-            case "scissorsrock": "scissorsrock"
-            case "paperscissors": "paperscissors"
-            cpuWinCount++
-                console.log(`Computer Wins ${p1WinCount}:${cpuWinCount}`);
-                break;
-            case "rockrock": "rockrock"
-            case "paperpaper": "paperpaper"
-            case "scissorsscissors": "scissorsscissors"
-                console.log(`TIE!! ${p1WinCount}:${cpuWinCount}`);
-                break;
+    i++
+    console.log(i)
+
+            if(p1 === cpu){}
+            else if(p1 =="rock" && cpu =="scissors" || p1 =="paper" && cpu=="rock" || p1=="scissors" && cpu=="paper"){
+                p1WinCount++;
+            }
+            else if(p1 =="scissors" && cpu =="rock" || p1=="rock" && cpu =="paper" || p1=="paper" && cpu=="scissors"){
+                cpuWinCount++;
+            }
+               console.log(`${p1WinCount}:${cpuWinCount}`)
+        }
+        // giving buttons choices
+         rock.addEventListener('click', () =>{
+            rps("rock");})
+         paper.addEventListener('click', () =>{
+            rps("paper");})
+         scissors.addEventListener('click', () =>{
+            rps("scissors");})
+           
+            if(p1WinCount > cpuWinCount){
+                console.log("you win")
+            }
+            else if(p1WinCount < cpuWinCount){
+                console.log("you lose")
+            }
+            else{console.log("tie game")}    
+        
     
-        }
-        }
-}
-rps()
