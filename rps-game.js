a47ad848bce89aa7558c07f599a9606de4925aa4
makyia bonner <makyiabonner@gmail.com>
2022-11-07 03:49:36 -0500

    reseting count when game ends

--- rps-game.js
+++ rps-game.js
@@ -3,6 +3,8 @@ const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
 const score = document.getElementById("score")
+const p1Choice = document.getElementById("player-name")
+const cpuChoice = document.getElementById("computer-choice")
 
 let cpuWinCount = 0
 let p1WinCount = 0
@@ -18,26 +20,31 @@ function rps(p1,cpu){
     if(p1 === cpu){}
     else if(p1 =="rock" && cpu =="scissors" || p1 =="paper" && cpu=="rock" || p1=="scissors" && cpu=="paper"){
       p1WinCount++;
+
+
     }
     else if(p1 =="scissors" && cpu =="rock" || p1=="rock" && cpu =="paper" || p1=="paper" && cpu=="scissors"){
          cpuWinCount++;
     }
+    
     score.textContent = p1WinCount + "|" + cpuWinCount;
-    if(p1WinCount === 5 && p1WinCount > cpuWinCount){
+    if(p1WinCount === 3 && p1WinCount > cpuWinCount){
         score.textContent = "you win";
+        p1WinCount = 0;
+        cpuWinCount = 0;
     }
-    else if(cpuWinCount === 5 && p1WinCount < cpuWinCount){
+    else if(cpuWinCount === 3 && p1WinCount < cpuWinCount){
         score.textContent = "you lose";
+        p1WinCount = 0;
+        cpuWinCount = 0;
+
         }
-        return score.textContent;
+        return score.textContent
+
+        
     }    
 
     
-    //round count and rules
-        
-               
-           
-        
 // giving buttons choices
          rock.addEventListener('click', () =>{
             rps("rock");})
